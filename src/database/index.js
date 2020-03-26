64b391ca5a24d4834873b541b9833e3fe716ddef
Vinicius Canuto <vinicanuto@github.com>
2020-03-26 01:02:00 -0300

    Criada rotas para listar armas de crimes e informação de crime

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ class Database {
     this.connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: 'V1canuto.',
+      password: '',
       database: 'police'
     });
 
